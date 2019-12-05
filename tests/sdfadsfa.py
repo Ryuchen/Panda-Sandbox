f994cd340a38c616739cd961c6cfb3a5ac842fc2
chenhao <chenhao@geyecloud.com>
2019-12-05 13:21:55 +0800

    test with git hooks pre-commit

---
+++ tests/sdfadsfa.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# ========================================================
+# @Author: Ryuchen
+# @Time: 2019/12/05-13:19
+# @Site: https://ryuchen.github.io
+# @Contact: chenhaom1993@hotmail.com
+# @Copyright: Copyright (C) 2019-2020 Panda-Sandbox.
+# ========================================================
+"""
+...
+DocString Here
+...
+"""
